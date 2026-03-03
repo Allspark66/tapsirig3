6b065497c137c43d959a59d7b0815eb976690b67
imam <147700388+Allspark66@users.noreply.github.com>
2026-03-03 22:34:47 +0400

    task3

---
+++ Task/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.Sqlite;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Task.Models;
+
+namespace Task.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Product> products = new List<Product>();
+
+            using (var connection = new SqliteConnection("Data Source=products.db"))
+            {
+                connection.Open();
+                var command = connection.CreateCommand();
+                command.CommandText = "SELECT title, price, image FROM products";
+
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        products.Add(new Product
+                        {
+                            Title = reader.GetString(0),
+                            Price = reader.GetDouble(1),
+                            Image = reader.IsDBNull(2) ? "default.jpg" : reader.GetString(2)
+                        });
+                    }
+                }
+            }
+            return View(products);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Task/Models/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace Task.Models
+{
+    public class Product
+    {
+        public string Title { get; set; }
+        public double Price { get; set; }
+        public string Image { get; set; } // Bunu əlavə et
+    }
+}
